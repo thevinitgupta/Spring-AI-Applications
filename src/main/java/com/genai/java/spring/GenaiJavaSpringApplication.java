466852f4b96f2cd4a3c9ab29cce5ff1679eb005e
thevinitgupta <thevinitgupta@gmail.cim>
2026-04-26 15:51:10 +0530

    first commit

---
+++ src/main/java/com/genai/java/spring/GenaiJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.genai.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenaiJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenaiJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genai/java/spring/GenaiJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genai.java.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenaiJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
